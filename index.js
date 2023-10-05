22b1a81be4f7ea81334c8b49d6ae940c3319a283
Cado <cado@Cados-MacBook-Air.local>
2023-10-05 15:49:13 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const nums = [3, 5, 7, 2]  // This is an arrays of numbers 
+const  nums2 = [3, 4, 6, 4 ]  // This is also an arrays of numbers 
+
+// console.log(nums[2]) // This will output the number 7
+
+let fruits = ['bananas', 'apple' , 'kiwi']  // This is an arrays of strings
+
+console.log(fruits[1]) // This will output apple
+
+fruits[0] = "pineapple" // Change the value of the first item to "pineapple"
+
+console.log(fruits[0]) // This will output pineapple 
+
+// This array has different data types in it 
+let things = [6, "Todd", false, "false", [2,5]] 
+
+console.log(things[4])
